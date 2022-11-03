3979a81a2fe213df0469df56244b0b3e7b5ee4e7
Adam Eade <105811017+AdamEade@users.noreply.github.com>
2022-11-03 20:52:03 +0000

    Add files via upload (#6)

---
+++ forum.js
@@ -0,0 +1,13 @@
+function create_post() {
+	document.getElementById("post-overlay").style.display = "flex";
+    document.getElementById("post-overlay-input").focus();
+};
+
+function cancel_button() {
+	document.getElementById("post-overlay").style.display = "none";
+};
+
+
+function filter_posts() {
+	
+};
