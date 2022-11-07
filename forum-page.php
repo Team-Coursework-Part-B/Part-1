9e4c0eadc5bf9c7e78ddbcd417e5d180ba133269
Gabuwu Bwown <90155922+froffyman@users.noreply.github.com>
2022-11-07 11:16:10 +0000

    Update and rename forum-page.html to forum-page.php

--- forum-page.php
+++ forum-page.php
@@ -13,9 +13,23 @@
 				<a class="nav-link", href="page.html">Task List</a>
 		    <a class="nav-link", href="project.html">Projet List</a>
 		    <a class="nav-link", id="active">Forum</a>
+		    <a class="nav-link", id="manager-navbar" href="manager.html">Manager Dashboard</a>
 		</div>
 	</nav>
 
+	 <?php
+
+$isManager = $_POST["email"];
+if  ($isManager == "manager@email.com") {
+	echo '<style type="text/css">
+		#manager-navbar {
+            display: inline;
+        }
+        </style>';
+}	
+
+ ?>
+	
 	<main>
 		<div class="kanban", id="kanban-forum">
       <div class="kanban-title" id="forum-title"><h1><b>Forum</b></h1></div>
